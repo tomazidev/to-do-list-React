a9c4175485a0d4d9d7ce12bd5bc864f907affca3
Tomasz Koprowski <tomasz.koprowski@talktalkplc.com>
2019-01-05 00:54:16 +0000

    adding components and scss

---
+++ src/component/Note/Note.js
@@ -0,0 +1,14 @@
+import React ,{ Component }  from 'react';
+import './Note.scss';
+
+class Note extends Component {
+    render() {
+        return (
+            <div className="">
+                <p>I am a NOTE</p>
+            </div>
+        )
+    }
+}
+
+export default Note;
